718fd81f27c33e3acdf92ac6bdcbd2b869edb1c8
WORK <USER@10.0.177.150>
2019-04-24 17:17:42 +0300

    add Example

---
+++ src/Main_Program/Main.java
@@ -0,0 +1,73 @@
+package Main_Program;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.SwingConstants;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Main extends Frame {
+
+	Main() {
+		// setLayout(new GridLayout(1, 5));
+		// setLayout(new FlowLayout());
+		setLayout(null);
+
+		Button b = new Button("Button");
+		b.setBounds(50, 70, 50, 20);
+		add(b);
+
+		JLabel l = new JLabel("Label", SwingConstants.CENTER);
+		l.setBounds(110, 70, 50, 20);
+		add(l);
+
+		JLabel ent = new JLabel("", SwingConstants.CENTER); // Enter
+		ent.setBounds(240, 100, 100, 20);
+		add(ent);
+
+		Checkbox c = new Checkbox("Choice");
+		c.setBounds(170, 70, 60, 20);
+		add(c);
+
+		Choice ch = new Choice();
+		for (int i = 1; i < 6; i++) {
+			ch.add(i + " ");
+		}
+		ch.setBounds(240, 70, 40, 20);
+		add(ch);
+
+		TextField tf = new TextField("");
+		tf.setBounds(50, 100, 100, 20);
+		add(tf);
+
+		setSize(400, 200);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		setVisible(true);
+
+		tf.addKeyListener(new KeyAdapter() {
+
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyText(e.getKeyCode()) == "Enter")
+					ent.setText(tf.getText());
+			}
+
+		});
+		
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+	}
+}
