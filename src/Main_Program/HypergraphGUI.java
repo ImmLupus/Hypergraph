f2677948292e416cdca3abf6db8c9f6081ef0588
WORK <USER@SAP-MSK-NB-0073>
2019-05-06 10:23:20 +0300

    finish for create template

--- src/Main_Program/HypergraphGUI.java
+++ src/Main_Program/HypergraphGUI.java
@@ -7,19 +7,31 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileOutputStream;
 
 import javax.swing.AbstractAction;
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButtonMenuItem;
-import javax.swing.JSeparator;
 import javax.swing.JTextField;
 
-import Main_Program.ExampleCard.Pic;
-import javax.swing.ImageIcon;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.sun.media.sound.InvalidFormatException;
+import java.awt.Color;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
+import java.awt.Checkbox;
 
 public class HypergraphGUI {
 
@@ -45,6 +57,7 @@ public class HypergraphGUI {
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
+	JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -141,11 +154,93 @@ public class HypergraphGUI {
 		textField_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		textField_3.setColumns(10);
 		panel_1.add(textField_3);
+		
+		Checkbox checkbox = new Checkbox("Сгенерировать случайные веса");
+		checkbox.setFont(new Font("Dialog", Font.PLAIN, 18));
+		checkbox.setBounds(32, 242, 287, 27);
+		panel_1.add(checkbox);
 
 		JButton button = new JButton("Сгенерировать шаблон EXCEL");
-		button.setBounds(107, 252, 343, 33);
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				int K=Integer.parseInt(textField.getText());
+				int LR=Integer.parseInt(textField_1.getText());
+				int I=Integer.parseInt(textField_2.getText());
+				int Ii=Integer.parseInt(textField_3.getText());
+				//Create Template!!!
+				XSSFWorkbook wb = new XSSFWorkbook();
+				XSSFSheet sheet= wb.createSheet("Sheet1");
+				XSSFRow row=sheet.createRow(0);
+				XSSFCell cell = row.createCell(0);
+				cell.setCellValue("Количество критериев:");
+				cell = row.createCell(1);
+				cell.setCellValue(K);
+					
+				row = sheet.createRow(1);
+				cell = row.createCell(0);
+				cell.setCellValue("Количество вершин типа L/R:");
+				cell = row.createCell(1);
+				cell.setCellValue(LR);
+					
+				row = sheet.createRow(2);
+				cell = row.createCell(0);
+				cell.setCellValue("Количество вершин типа I:");
+				cell = row.createCell(1);
+				cell.setCellValue(I);
+					
+				row = sheet.createRow(3);
+				cell = row.createCell(0);
+				cell.setCellValue("Размерность вершин I[i] (через запятую):");
+				cell = row.createCell(1);
+				cell.setCellValue(Ii);
+				
+				for (int i=0,k=1; k<=K; i+=3,k++) {
+					XSSFRow row2 = sheet.createRow(4+i+1);
+					XSSFCell cell2 = row2.createCell(0);
+					cell2.setCellValue("Веса:");
+					XSSFRow row1 = sheet.createRow(4+i);
+					XSSFCell cell1 = row1.createCell(0);
+					cell1.setCellValue("Критерий №"+(k));
+					for (int l=1,count=1; l<=LR; l++) {
+						for (int r=1; r<=LR; r++) {
+							for (int j=1; j<=I; j++,count++) {
+								cell1=row1.createCell(count);
+								cell1.setCellValue(l+" "+r+" "+j);
+								if (checkbox.getState()){
+									cell2=row2.createCell(count);
+									cell2.setCellValue((int)(Math.random() * 10000));
+								}
+							}
+						}
+					}
+				}
+				
+				try {
+					FileOutputStream fileOut = new FileOutputStream("input.xlsx");
+			        wb.write(fileOut);
+			        fileOut.close();
+					wb.close();
+					
+					lblNewLabel.setText("Готово!");
+					lblNewLabel.setForeground(new Color(34, 139, 34));
+					lblNewLabel.setVisible(true);
+				} catch (Exception e) {
+					lblNewLabel.setText("Ошибка!");
+					lblNewLabel.setForeground(new Color(255, 0, 0));
+					lblNewLabel.setVisible(true);
+				}
+			}
+		});
+		button.setBounds(108, 275, 343, 33);
 		button.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		panel_1.add(button);
+		
+		lblNewLabel = new JLabel("New label");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(227, 324, 105, 31);
+		panel_1.add(lblNewLabel);
+		lblNewLabel.setVisible(false);
 
 		JPanel panel_2 = new JPanel();
 		panel.add(panel_2, "name_1475742599721723");
